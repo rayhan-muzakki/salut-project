fc692516d7481edafd0298e4f73f672ce7713546
rayhan muzakki <rayhanmuzakki1@gmail.com>
2021-03-02 20:38:51 +0700

    added functions

--- app/Models/staff.php
+++ app/Models/staff.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Staff extends Model
 {
+    public function salutOfc(){
+    	return $this->belongsTo(SalutOfc::class,'salut_ofc_id','id');
+    }
+
+    public function staffType(){
+    	return $this->belongsTo(StaffTipe::class,'staff_tipe_id','id');
+    }
     public $timestamps = false;
 
     protected $fillable = [
